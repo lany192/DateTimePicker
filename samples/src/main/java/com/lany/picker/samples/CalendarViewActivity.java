34f50244d69e3b1da0349a08ea0ad9345170743c
Lany <lany192@qq.com>
2016-11-01 09:49:42 +0800

    update

--- samples/src/main/java/com/lany/picker/samples/CalendarViewActivity.java
+++ samples/src/main/java/com/lany/picker/samples/CalendarViewActivity.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.lany.picker.calendarview.CalendarView;
+
 public class CalendarViewActivity extends AppCompatActivity {
 
+    private CalendarView mCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_calendar_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mCalendarView=(CalendarView)findViewById(R.id.calendarView);
+//        mCalendarView.setFocusedMonthDateColor(Color.parseColor("#555555"));
+//        mCalendarView.setSelectedWeekBackgroundColor(Color.parseColor("#80dfdfdf"));
+//        mCalendarView.setUnfocusedMonthDateColor(Color.parseColor("#bbbbbb"));
     }
 
     @Override
